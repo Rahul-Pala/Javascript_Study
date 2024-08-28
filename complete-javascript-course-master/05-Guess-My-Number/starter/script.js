93ffd3b5edaf7beffed6d01a1b526e014a6e99b1
Rahul-Pala <rahul_pala@hotmail.com>
2024-08-28 12:08:34 +1200

    Guess my name updated

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 // console.log(document.querySelector(`.message`).textContent);
 // document.querySelector(`.message`).textContent = "Correct Number?"
 
@@ -7,11 +7,21 @@
 
 // document.querySelector(`.guess`).value = 23
 // console.log(document.querySelector(`.guess`).value)
-const number = Math.random();
+//const number = Math.random();
 
-document.querySelector(`.check`).addEventListener
-(`click`, function () {
-console.log(document.querySelector(`.guess`).value)
-})
+// document.querySelector(`.check`).addEventListener
+// (`click`, function () {
+// console.log(document.querySelector(`.guess`).value)
+// })
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1 ;
+document.querySelector('.number').textContent = secretNumber
+console.log(secretNumber)
+
+document.querySelector('.check').addEventListener
+('click', function () {
+    const guess = Number(document.querySelector('.guess').value) ;
+    console.log(guess, typeof guess)
+}
+)
 
